996701a6b939cddc00c28ed990655da0729f11df
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-09 11:10:55 -0400

    Add dropdown menu with logout button

--- frontend/components/greeting/communities_dropdown.jsx
+++ frontend/components/greeting/communities_dropdown.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CommunitiesDropdown = ({className, logout}) => (
-  <div className={className}>
-    <a onClick={logout}>Log out</a>
+  <div className={`${className} communities-dropdown`}>
+    <h3><a>CURRENT COMMUNITY</a></h3>
+    <ul className="community">
+      <li>
+        <a><i className="so-icon"></i>Heap Overrun</a>
+        <ul className="community-links">
+          <li><a>help</a></li>
+          <li><a>chat</a></li>
+          <li><a onClick={logout}>log out</a></li>
+        </ul>        
+      </li>
+      <li>
+        <i className="l-icon"></i>
+        <a><i className="so-icon grayscale"></i> Meta Stack Overflow</a>
+      </li>
+    </ul>
+    <h3><a>YOUR COMMUNITIES</a></h3>
+    <ul className="community your-community">
+      <li>
+        <a><i className="so-icon"></i>Heap Overrun</a>
+      </li>
+    </ul>
   </div>
 );
 
